b5ee03a2ad990dec3cc0a7eb050e157ee1b99859
12leticiasilva0990 <130669115+12leticiasilva0990@users.noreply.github.com>
2023-08-17 14:37:08 -0300

    first commit

---
+++ main.js
@@ -0,0 +1,16 @@
+function checkButton() {
+    var date = new Date(document.getElementById("dateInput").value);
+    var resultElement = document.getElementById("result");
+  
+    var month = date.getMonth() + 1; // getMonth() returns 0-11
+  
+    if (month >= 3 && month <= 5) {
+      resultElement.textContent = "Primavera 🌷";
+    } else if (month >= 6 && month <= 8) {
+      resultElement.textContent = "Verao 😎";
+    } else if (month >= 9 && month <= 11) {
+      resultElement.textContent = "Outono 🍂";
+    } else {
+      resultElement.textContent = "Inverno 🥶";
+    }
+  }
